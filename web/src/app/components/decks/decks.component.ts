846f1eae1aa4544b041410ff7f8ef75ab1772c48
Jon Koenig <jkoenig@outlook.com>
2022-06-09 17:43:06 -0700

    Redesigning components

--- web/src/app/components/decks/decks.component.ts
+++ web/src/app/components/decks/decks.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DeckService } from '../../services/deck.service';
 import { Deck } from '../../Deck';
 
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
-
 @Component({
   selector: 'app-decks',
   templateUrl: './decks.component.html',
@@ -11,11 +9,19 @@ import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 })
 export class DecksComponent implements OnInit {
   decks: Deck[] = [];
-  faEllipsis = faEllipsis;
+  buttonTextControl: boolean = true
+  visibleControl: boolean = true;
+  visibleClasses: string = "decks-container-common decks-container";
+  hiddenClasses: string = "decks-container-common decks-container-hidden";
   
   constructor(private deckService: DeckService) { }
 
   ngOnInit(): void {
     this.deckService.getDecks().subscribe(decks => this.decks = decks);
+  }
+
+  handleMoreButtonClick() {
+    this.visibleControl = !this.visibleControl;
+    this.buttonTextControl = !this.buttonTextControl;
   }
 }
